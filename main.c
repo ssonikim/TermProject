b0996e416fa8bfd0c7619a9bffd67bf586981ac7
Do Hyoung Kim <kdh4714@naver.com>
2018-11-21 01:23:14 -0800

    dohyung main

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ int main(void)
 
 {
   printf("Hello Man~~\n");
-  printf("Hello I'm kwan dong~~why so serious?")
+  printf("Hello I'm kwan dong~~why so serious?");
+  printf("This program is do's main");
   return 0;
 }
