1826ac6cd0ae886e57264640dd71cf12f2539390
Kwandong Song <gvc4456@hanmail.net>
2018-11-24 06:48:36 -0800

    update main.c -kwandong-

--- main.c
+++ main.c
@@ -1,9 +1,34 @@
 #include<stdio.h>
 
-int main(void)
-
+int main()
 {
-  printf("Hello Man~~\n");
-  printf("Hello I'm kwan dong~~why so serious?")
+  int InputMenu; //for switch values
+  while(1)
+  {
+    printf("Plese select Menu what you want.\n"); //select
+    printf("1.Start!\n");  //start
+    printf("2.END!\n");  //end
+    scanf("%d",&InputMenu); //input number of menu
+    system("clear"); //clear
+    switch(InputMenu) 
+    {
+      case 1: //Start
+      {
+        printf("gogogo\n");
+        break;
+      }
+      case 2: //End
+      {
+        printf("byebyebye\n");
+        return 0;
+      }
+      default: //something wrong
+      {
+        printf("you have entered something wrong!\n try again please\n");
+        break;
+      }
+    }
+    system("clear");
+  }
   return 0;
 }
