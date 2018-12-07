71a0c7f360ba7ec652338d5c7e63b6b389ca72e2
Do Hyoung Kim <kdh4714@naver.com>
2018-12-07 07:33:29 -0800

    input delete mode -do-

--- main-lib/erase.c
+++ main-lib/erase.c
@@ -1,19 +1,50 @@
 #include <sqlite3.h>
 #include <stdio.h>
+#include <string.h>
 #include "global.h"
+char date1[10];
+char name1[20];
+//int iprice=0;
+char query1[]="delete from AccBook where date = \'";
+char str1[100];
+char dda1[]="\'";
+char mid[] = "and name = \'";
 
+void make_sql1()
+{
+  //put date to char date
+  printf("put date:\n");
+  scanf("%s",date1);
+  //put date to char name
+  printf("put name:\n");
+  scanf("%s",name1);
+
+  strcat(str1,query1);
+  strcat(str1,date1);
+  strcat(str1,dda1);
+  strcat(str1,mid);
+  strcat(str1,name1);
+  strcat(str1,dda1);
+  printf("%s\n",str1);
+}
 int erase()
 {
+  make_sql1();
 
-  sql = "delete from AccBook where date = '1.1'and name ='apple'";
-  rc = sqlite3_exec(db,sql,NULL,NULL,&zErr);
+  rc = sqlite3_exec(db,str1,NULL,NULL,&zErr);
 
    if (SQLITE_OK != rc){
 		fprintf(stderr,"rc=%d\n",rc);
 		fprintf(stderr,"sqlite3_remove error : %s\n",sqlite3_errmsg(db));
 		printf("database remove value error\n");
 		return -1;
+  		       }
+  //reset
+    for(int i=0;i<100;i++)
+  {
+    str1[i]=0;
   }
+  
 
 
 
--- main-lib/write.c
+++ main-lib/write.c
@@ -1,39 +1,61 @@
+
 #include <sqlite3.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "global.h"
+#include <string.h>
+
+char date[10];
+char name[20];
+int iprice=0;
+char price[20];
+char query[]="insert into Accbook values(\'";
+char str[100];
+char ddapoint[]="\',";
+char dda[]="\'";
+char last[]="\');";
+
+void make_sql()
+{
+  //put date to char date
+  printf("put date:\n");
+  scanf("%s",date);
+  //put date to char name
+  printf("put name:\n");
+  scanf("%s",name);
+  //put date to char price
+  printf("put price:\n");
+  scanf("%s",price);
+
+  strcat(str,query);
+  strcat(str,date);
+  strcat(str,ddapoint);
+  strcat(str,dda);
+  strcat(str,name);
+  strcat(str,ddapoint);
+  strcat(str,dda);
+  strcat(str,price);
+  strcat(str,last);
+  //check str
+  printf("%s\n",str);
+}
+
 int write()
 {
-//  char date_in[100];
-//  char name_in[100];
-//  char price_in[100];
-
-//  printf("Type Date : Product Name: Price\n");
-
-  //scanf("%s %s %s",date,name,price);
-
-//  scanf("%s %s %s",&date_in,&name_in,&price_in);
-  sql = "insert into AccBook values('1.1','apple',500);";
-
-  //sql = "insert into AccBook values(date,name,price);";
-  rc = sqlite3_exec(db,sql,NULL,NULL,&zErr);
-
-   if (SQLITE_OK != rc){
-		fprintf(stderr,"rc=%d\n",rc);
-		fprintf(stderr,"sqlite3_write value error : %s\n",sqlite3_errmsg(db));
-		printf("database write error\n");
-		return -1;
-			}
-  sql = "insert into AccBook values('1.2','banana',500);";
-
-  rc = sqlite3_exec(db,sql,NULL,NULL,&zErr);
-
+  make_sql();
+  rc = sqlite3_exec(db,str,NULL,NULL,&zErr);
+ 
    if (SQLITE_OK != rc){
 		fprintf(stderr,"rc=%d\n",rc);
 		fprintf(stderr,"sqlite3_write value error : %s\n",sqlite3_errmsg(db));
 		printf("database write close error\n");
 		return -1;
-  			}
+  }
+  //reset
+  for(int i=0;i<100;i++)
+  {
+    str[i]=0;
+  }
 
-
-return 0;
+  return 0;
 }
